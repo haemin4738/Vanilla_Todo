33733ab3381a82332d0b9c4cd887c2aa5436dc1f
haemin4738 <leeheamin12@gmail.com>
2025-07-07 10:20:33 +0900

    fix : innerHTMl 하드코딩

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import TodoForm from "./components/TodoForm.js";
+import TodoList from "./components/TodoList.js";
+
 function App({ $target }) {
   const $div = document.createElement("div");
   $target.appendChild($div);
   console.log($target);
 
-  new TodoForm();
-  new TodoList();
+  new TodoForm({ $target: $div });
+  new TodoList({ $target: $div });
 }
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,3 +1,7 @@
-function TodoList() {}
+function TodoList({ $target }) {
+  const $list = document.createElement("list");
+  $list.innerHTML = "<ul><li>씻기</li><li>걷기</li><li>밥 먹기</li><ul>";
+  $target.appendChild($list);
+}
 
 export default TodoList;
