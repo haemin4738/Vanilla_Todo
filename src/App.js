d788cc2e9f9deb16a0b471361f572fe5893fc245
haemin4738 <leeheamin12@gmail.com>
2025-07-07 11:36:15 +0900

    fix : TodoForm.js 수정

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import TodoForm from "./components/TodoForm.js";
 import TodoList from "./components/TodoList.js";
 
 function App({ $target }) {
-  const $div = document.createElement("div");
-  $target.appendChild($div);
+  const intialState = [
+    { id: 1, text: "item1", checked: true },
+    { id: 2, text: "item2", checked: false },
+    { id: 3, text: "item3", checked: false },
+  ];
 
-  const header = new Header({ $target: $div });
+  const onSubmit = (text) => {
+    const nextState = [...todoList.state, text];
+    todoList.setState(nextState);
+  };
+
+  const $page = document.createElement("div");
+  $target.appendChild($page);
+
+  const header = new Header({ $target: $page });
   header.setState("!!오늘 할일!!");
 
-  new TodoForm({ $target: $div });
+  new TodoForm({ $target: $page, onSubmit });
 
-  const intialState = ["할일1", "할일2", "할일3"];
-  const list = new TodoList({ $target: $div, intialState });
-  list.setState([...intialState, "할일4"]);
+  const todoList = new TodoList({ $target: $page, intialState });
 }
 
 export default App;
