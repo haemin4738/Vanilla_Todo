bc0372c549d0d348320fd14fae8ec6c4f99f38bb
haemin4738 <leeheamin12@gmail.com>
2025-07-07 09:51:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 function App({ $target }) {
+  const $div = document.createElement("div");
+  $target.appendChild($div);
   console.log($target);
+
+  new TodoForm();
+  new TodoList();
 }
 export default App;
